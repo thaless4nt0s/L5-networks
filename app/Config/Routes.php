eb94025956d5082e6f46b97696408bac4a93b58b
thales santos <thales.infor144@gmail.com>
2025-02-25 15:31:03 -0300

    feat: cria a rota POST /admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->group('admin/', function ($routes) {
+    $routes->post('criar', 'AdminController::adicionarAdministrador');
+});
