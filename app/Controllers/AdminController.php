87938e73b7a41c3b79cfd643ff46fcf148b00e49
thales santos <thales.infor144@gmail.com>
2025-02-25 15:33:53 -0300

    feat: Cria a função responsavel pela rota POST /admin/criar

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -13,4 +13,33 @@ class AdminController extends BaseController
     {
         $this->adminRepository = new AdminRepository();
     }
+
+    public function adicionarAdministrador()
+    {
+        $dados = [
+            'nome' => $this->request->getVar('nome'),
+            'email' => $this->request->getVar('email'),
+            'senha' => password_hash($this->request->getVar('senha'), PASSWORD_DEFAULT),
+        ];
+        // Criar administrador
+        try {
+            $admin = $this->adminRepository->criarAdministrador($dados);
+            if ($admin) {
+                return $this->response->setJSON([
+                    'message' => 'Administrador criado com sucesso!',
+                    'statusCode' => 201, // Usar 201 para criação bem-sucedida
+                ]);
+            } else {
+                return $this->response->setJSON([
+                    'message' => 'Falha ao criar administrador',
+                    'statusCode' => 500 // Usar 500 para erros internos do servidor
+                ]);
+            }
+        } catch (\Exception $e) {
+            return $this->response->setJSON([
+                'message' => 'Erro ao criar administrador: ' . $e->getMessage(),
+                'statusCode' => 500
+            ]);
+        }
+    }
 }
