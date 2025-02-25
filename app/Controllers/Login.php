1ae04629985654323e48fcfe6dee5f90d913e1c3
thales santos <thales.infor144@gmail.com>
2025-02-25 17:32:24 -0300

    feat: Adiciona a rota de login /POST /admin/login

---
+++ app/Controllers/Login.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\AdminModel;
+use CodeIgniter\API\ResponseTrait;
+use \Firebase\JWT\JWT;
+
+class Login extends BaseController
+{
+    use ResponseTrait;
+
+    public function index()
+    {
+        $adminModel = new AdminModel();
+
+        $email = $this->request->getVar('email');
+        $password = $this->request->getVar('senha');
+
+        $user = $adminModel->where('email', $email)->first();
+        if (is_null($user)) {
+            return $this->respond(['error' => 'Usuário ou senha inválidos.'], 401);
+        }
+
+        $pwd_verify = password_verify($password, $user['senha']);
+        if (!$pwd_verify) {
+            return $this->respond(['error' => 'Usuário ou senha inválidos.'], 401);
+        }
+
+        // Verifique e defina a chave secreta
+        $key = env('JWT_SECRET', 'PV2%IsQZ*P2>&&F');
+        if (empty($key)) {
+            return $this->respond(['error' => 'Chave de login não está configurada.'], 500);
+        }
+
+        $iat = time(); // current timestamp value
+        $exp = $iat + 3600;
+
+        $payload = array(
+            "iss" => "Issuer of the JWT",
+            "aud" => "Audience that the JWT",
+            "sub" => "Subject of the JWT",
+            "iat" => $iat, //Time the JWT issued at
+            "exp" => $exp, // Expiration time of token
+            "email" => $user['email'],
+        );
+
+        try {
+            $token = JWT::encode($payload, $key, 'HS256');
+        } catch (\Exception $e) {
+            return $this->respond(['error' => 'Token generation failed: ' . $e->getMessage()], 500);
+        }
+
+        $response = [
+            'message' => 'Login Successful',
+            'token' => $token
+        ];
+
+        return $this->response->setJSON([
+            'response' => $response,
+            'statusCode' => 200
+        ]);
+    }
+
+}
