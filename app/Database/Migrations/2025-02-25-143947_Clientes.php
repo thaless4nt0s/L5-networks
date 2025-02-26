390d9bdbde95ebff52656b4f61ddc4e68cabfbc9
thales santos <thales.infor144@gmail.com>
2025-02-26 14:14:27 -0300

    fix: Ajusta a chave primaria para id ao inves de cpf

--- app/Database/Migrations/2025-02-25-143947_Clientes.php
+++ app/Database/Migrations/2025-02-25-143947_Clientes.php
@@ -9,6 +9,11 @@ class Clientes extends Migration
     public function up()
     {
         $this->forge->addField([
+            'id' => [
+                'type' => "INT",
+                'AUTO_INCREMENT' => true,
+                'unsigned' => true
+            ],
             'cpf' => [
                 'type' => 'VARCHAR',
                 'constraint' => '11'
@@ -16,14 +21,11 @@ class Clientes extends Migration
             'nome' => [
                 'type' => 'VARCHAR',
                 'constraint' => '50'
-            ],
-            'senha' => [
-                'type' => 'VARCHAR',
-                'constraint' => '50'
             ]
         ]);
 
-        $this->forge->addPrimaryKey('cpf');
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addUniqueKey('cpf');
         $this->forge->createTable('clientes', true, ['engine' => 'innodb']);
     }
 
